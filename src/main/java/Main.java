a3fde1a78a63ef36cdffa921ecd2066cf42f0e51
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-12-17 18:26:33 +0100

    Implementation of servlet and jetty

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,34 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("It's just a start...");
+
+    public static void main(String[] args) throws Exception {
+        WebAppContext webAppContext = new WebAppContext();
+//        webAppContext.addServlet(HelloServlet.class, "/api/*");
+        webAppContext.setResourceBase("src/main/webapp");
+        webAppContext.setContextPath("/");
+
+        webAppContext.setConfigurations(new Configuration[]{
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new MetaInfConfiguration(),
+                new FragmentConfiguration(),
+                new EnvConfiguration(),
+                new PlusConfiguration(),
+                new JettyWebXmlConfiguration()
+        });
+
+        webAppContext.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+
+        Server server = new Server(8080);
+        server.setHandler(webAppContext);
+
+        server.start();
+        server.join();
     }
 }
