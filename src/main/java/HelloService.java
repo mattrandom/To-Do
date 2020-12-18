730c8c8943e88333647bde38909b3bfa8c00427d
mattrandom <44298255+mattrandom@users.noreply.github.com>
2020-12-18 11:44:21 +0100

    Implementation of Service & Refactoring Servlet

---
+++ src/main/java/HelloService.java
@@ -0,0 +1,9 @@
+import java.util.Optional;
+
+class HelloService {
+    static final String FALLBACK_NAME = "world";
+
+    String prepareGreeting(String name) {
+        return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + "!";
+    }
+}
