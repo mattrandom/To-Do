3cffe494e36f411e58662e83f771f11ddcd74b12
mattrandom <44298255+mattrandom@users.noreply.github.com>
2021-01-05 13:48:25 +0100

    Hibernate usage in 'repository'

--- src/main/java/LangRepository.java
+++ src/main/java/LangRepository.java
@@ -1,19 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import java.util.Optional;
 
 public class LangRepository {
-    private List<Lang> languages;
-
-    LangRepository() {
-        languages = new ArrayList<>();
-        languages.add(new Lang(1, "Hello", "en"));
-        languages.add(new Lang(2, "Witaj", "pl"));
-    }
 
     Optional<Lang> findById(Integer id) {
-        return languages.stream()
-                .filter(lang -> lang.getId().equals(id))
-                .findFirst();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Lang result = session.get(Lang.class, id);
+
+        transaction.commit();
+        session.close();
+
+        return Optional.ofNullable(result);
     }
 }
