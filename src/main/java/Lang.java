b1d74d3bc85fbb3a511f356f23a8e2e3fe01ac70
mattrandom <44298255+mattrandom@users.noreply.github.com>
2021-01-05 13:02:53 +0100

    Adaptation of 'model' to work with Hibernate

--- src/main/java/Lang.java
+++ src/main/java/Lang.java
@@ -1,7 +1,26 @@
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "languages")
 class Lang {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
     private Integer id;
     private String welcomeMsg;
     private String code;
+
+    /**
+     * Hibernate (JPA) needs it.
+     */
+    @SuppressWarnings("unused")
+    Lang() {
+    }
 
     public Lang(Integer id, String welcomeMsg, String code) {
         this.id = id;
