6dc9f5dd1cf34230475c5f0fb8fa11b67b347cbb
mattrandom <44298255+mattrandom@users.noreply.github.com>
2021-01-06 15:40:57 +0100

    Add method LangRepository.findAll

--- src/main/java/LangRepository.java
+++ src/main/java/LangRepository.java
@@ -1,9 +1,22 @@
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
 import java.util.Optional;
 
 public class LangRepository {
+
+    List<Lang> findAll() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<Lang> result = session.createQuery("from Lang", Lang.class).list();
+
+        transaction.commit();
+        session.close();
+
+        return result;
+    }
 
     Optional<Lang> findById(Integer id) {
         Session session = HibernateUtil.getSessionFactory().openSession();
