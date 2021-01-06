7efb6b4c0fd57ece5e8dab594337d757eab18702
mattrandom <44298255+mattrandom@users.noreply.github.com>
2021-01-06 22:09:44 +0100

    Add LangDTO & LangService & LangServlet refactor

---
+++ src/main/java/io/github/mattrandom/lang/LangDTO.java
@@ -0,0 +1,23 @@
+package io.github.mattrandom.lang;
+
+class LangDTO {
+    private Integer id;
+    private String code;
+
+    LangDTO(Lang lang) {
+        this.id = lang.getId();
+        this.code = lang.getCode();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/io/github/mattrandom/lang/LangService.java
@@ -0,0 +1,24 @@
+package io.github.mattrandom.lang;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class LangService {
+    private LangRepository repository;
+
+    LangService() {
+        this(new LangRepository());
+    }
+
+    LangService(LangRepository repository) {
+        this.repository = repository;
+    }
+
+    List<LangDTO> findAll() {
+        return repository
+                .findAll()
+                .stream()
+                .map(LangDTO::new)
+                .collect(Collectors.toList());
+    }
+}
