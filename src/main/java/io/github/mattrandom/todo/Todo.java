6e9c998ae61ad98e0587e737ed9f06c42f22cfaf
mattrandom <44298255+mattrandom@users.noreply.github.com>
2021-01-07 14:47:39 +0100

    Another DB migrations & new entity called Todo

---
+++ src/main/java/io/github/mattrandom/todo/Todo.java
@@ -0,0 +1,50 @@
+package io.github.mattrandom.todo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+class Todo {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
+    private String text;
+    private boolean done;
+
+    /**
+     * Hibernate (JPA) needs it.
+     */
+    @SuppressWarnings("unused")
+    Todo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/main/resources/db/migration/V5__Add_todos.sql
@@ -0,0 +1,3 @@
+insert into todos (text, done) values ('Done TODO', 1);
+insert into todos (text, done) values ('Undone TODO', 0);
+
